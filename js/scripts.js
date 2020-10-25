d4919a5cd01b58a7a683289d1f8380a0de8b98a7
Mojca Bensa <mojca.bensa@gmail.com>
2020-10-25 15:04:21 +0100

    added charts and notification box

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@
  *************************************/
 
 const alertBanner = document.getElementById("alert");
+const dot = document.querySelector(".dot");
 alertBanner.innerHTML = `
 <div class="alert-banner">
 <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete.</p>
@@ -14,11 +15,34 @@ alertBanner.addEventListener("click", (e) => {
   const element = e.target;
   if (element.classList.contains("alert-banner-close")) {
     alertBanner.style.display = "none";
+    dot.style.display = "none";
   }
 });
 
 /*************************************
- **********TRAFFIC BANNER*************
+ *********NOTIFICATION BOX************
+ *************************************/
+
+const popUp = document.getElementById("pop-up-notification");
+const span = document.querySelector(".close");
+const bell = document.querySelector("#notifications");
+
+bell.onclick = function () {
+  popUp.style.display = "block";
+};
+
+span.onclick = function () {
+  popUp.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == popUp) {
+    popUp.style.display = "none";
+  }
+};
+
+/*************************************
+ **********TRAFFIC CHART*************
  *************************************/
 
 const trafficCanvas = document.getElementById("traffic-chart");
@@ -66,4 +90,97 @@ let trafficChart = new Chart(trafficCanvas, {
   type: "line",
   data: trafficData,
   options: trafficOptions,
+});
+
+/*************************************
+ ********DAILY TRAFFIC CHART**********
+ *************************************/
+
+const dailyCanvas = document.getElementById("daily-chart");
+
+const dailyData = {
+  labels: ["S", "M", "T", "W", "T", "F", "S"],
+  datasets: [
+    {
+      label: "# of Hits",
+      data: [75, 115, 175, 125, 225, 200, 100],
+      backgroundColor: "#7477BF",
+      borderWidth: 1,
+    },
+  ],
+};
+
+const dailyOptions = {
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+    ],
+  },
+  legend: {
+    display: false,
+  },
+};
+
+let dailyChart = new Chart(dailyCanvas, {
+  type: "bar",
+  data: dailyData,
+  options: dailyOptions,
+});
+
+/*************************************
+ ********MOBIEL USERS CHART***********
+ *************************************/
+
+const mobileCanvas = document.getElementById("mobile-users-chart");
+
+const mobileData = {
+  labels: ["Desktop", "Tablet", "Phones"],
+  datasets: [
+    {
+      label: "# of Users",
+      data: [2000, 550, 500],
+      borderWidth: 0,
+      backgroundColor: ["#7477BF", "#78CF82", "#51B6C8"],
+    },
+  ],
+};
+
+const mobileOptions = {
+  legend: {
+    position: "right",
+    labels: {
+      boxWidth: 20,
+      fontStyle: "bold",
+    },
+  },
+};
+
+let mobileChart = new Chart(mobileCanvas, {
+  type: "doughnut",
+  data: mobileData,
+  options: mobileOptions,
+});
+
+/*************************************
+ ********MESSAGING SECTION************
+ *************************************/
+
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
+
+send.addEventListener("click", () => {
+  if (user.value === "" && message.value === "") {
+    alert("Please fill out user and message fields before sending");
+  } else if (user.value === "") {
+    alert("Please fill out user field before sending");
+  } else if (message.value === "") {
+    alert("Please fill out message field before sending");
+  } else {
+    alert(`Message successfully sent to: ${user.value}`);
+  }
 });
